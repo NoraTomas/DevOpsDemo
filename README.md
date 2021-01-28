a131dc3b3458ca9268e171f40527c592b64d1c74
NoraTomas <nora.tomas@vipps.no>
2021-01-28 19:41:53 +0100

    Create basic API endpoint

---
+++ README.md
@@ -0,0 +1,5 @@
+# DevOpsDemo
+
+This is a demo project to learn how a pipeline works. 
+Try deploying the demo to an AWS EC2 instance by using AWS CodePipeline, 
+CodeBuild and CodeDeploy. 
---
+++ src/main/java/com/example/restservice/WeatherController.java
@@ -0,0 +1,13 @@
+package com.example.restservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WeatherController {
+
+	@GetMapping("/")
+	public String greeting() {
+		return ("The temperature in Oslo is -4 degrees");
+	}
+}
