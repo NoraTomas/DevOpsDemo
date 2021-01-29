4c8a0b7c2e0e72f7eac96d29d43d39a4ba03f85e
NoraTomas <nora.tomas@vipps.no>
2021-01-29 21:47:48 +0100

    Give correct name to JAR file

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar -Dspring.profiles.active=dev /home/ec2-user/justiceleague-tracker-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+java -jar -Dspring.profiles.active=dev /home/ec2-user/rest-service-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
