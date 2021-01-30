257519a499755254dce8fe69fd6b68850475a6eb
NoraTomas <nora.tomas@vipps.no>
2021-01-30 15:07:51 +0100

    Add cleanup script

---
+++ scripts/cleanup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /home/ec2-user/
+rm appspec.yml
+rm rest-service-0.0.1-SNAPSHOT-spring-boot.jar
+rm rest-service-0.0.1-SNAPSHOT.jar
+rm stop_server.sh
+rm start_server.sh
+rm fix_previleges.sh
+exit 0
